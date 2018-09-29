ae5a2e289ef65a2f2cd70c21d0317b9e5c90467f
Tommaso Ricci <zorgatone@gmail.com>
2018-09-29 21:13:22 +0200

    first commit

---
+++ src/containers/Persons.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+
+import Person from "../components/Person/Person";
+import AddPerson from "../components/AddPerson/AddPerson";
+
+class Persons extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      persons: []
+    };
+
+    this.personAddedHandler = this.personAddedHandler.bind(this);
+    this.personDeletedHandler = this.personDeletedHandler.bind(this);
+  }
+
+  personAddedHandler() {
+    const newPerson = {
+      id: new Date().getTime(), // not really unique but good enough here!
+      name: "Max",
+      age: Math.floor(Math.random() * 40)
+    };
+
+    this.setState(prevState => {
+      return { persons: prevState.persons.concat(newPerson) };
+    });
+  }
+
+  personDeletedHandler(personId) {
+    this.setState(prevState => {
+      return {
+        persons: prevState.persons.filter(person => person.id !== personId)
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <AddPerson personAdded={this.personAddedHandler} />
+        {this.state.persons.map(person => (
+          <Person
+            key={person.id}
+            name={person.name}
+            age={person.age}
+            clicked={() => this.personDeletedHandler(person.id)}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Persons;
